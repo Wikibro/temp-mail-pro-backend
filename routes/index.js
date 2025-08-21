526611f17e0690a554c27109c836120e1a0c85c5
waqas <waqasali5991@gmail.com>
2025-08-21 15:35:45 +0500

    Initial backend code commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ const router = express.Router();
 router.use('/accounts', accountRouter);
 router.use('/inbox', inboxRouter);
 
+router.get('/', (req, res) => {
+  res.send('API working');
+})
 export default router;
--- server.js
+++ server.js
@@ -4,9 +4,21 @@ import cors from 'cors';
 import apiRoutes from './routes/index.js';
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+// app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'https://temp-mail-pro-frontend.vercel.app',
+        'https://your-custom-domain.com'
+    ]
+}));
+
+
 app.use(express.json());
 app.use('/api', apiRoutes);
+app.get('/', (req, res) => {
+    res.send('Temp Mail Pro Backend is running');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
