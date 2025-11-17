ff7e2f1c16d801516988123516130787d7b78ed0
waqas ali <97301126+Wikibro@users.noreply.github.com>
2025-11-17 23:54:31 +0500

    Remove sitemap.xml serving endpoint
    Removed the endpoint that served sitemap.xml directly from the backend.

--- server.js
+++ server.js
@@ -31,51 +31,7 @@ app.use(express.json());
 
 app.use('/api', apiRoutes);
 
-// ✅ NEW: Serve sitemap.xml directly from backend (no script injection)
-app.get('/sitemap.xml', (req, res) => {
-    const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://tempmailpk.com/</loc>
-    <lastmod>2025-11-16</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://tempmailpk.com/app</loc>
-    <lastmod>2025-11-16</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>https://tempmailpk.com/blog/tech-behind-disposable-emails</loc>
-    <lastmod>2025-11-15</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>https://tempmailpk.com/blog/receive-sms-otp-online</loc>
-    <lastmod>2025-11-15</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>https://tempmailpk.com/blog/private-domains-temp-email</loc>
-    <lastmod>2025-11-15</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  <url>
-    <loc>https://tempmailpk.com/blog/burner-email-for-social-media</loc>
-    <lastmod>2025-11-15</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-</urlset>`;
-    
-    res.set('Content-Type', 'application/xml');
-    res.send(sitemapContent);
-});
+
 
 app.get('/', (req, res) => {
     res.send('Temp Mail Pro Backend is running');
